85ba504fe069e75f9d1bd5d51e4ad7ac6bc81b51
ijabir28 <jabir.kamal@northsouth.edu>
2022-04-19 20:03:36 +0600

    testing tool implemented for packages

--- server/test/e2e/package.test.js
+++ server/test/e2e/package.test.js
@@ -3,10 +3,16 @@ const request = require('supertest');
 const {create_app} = require("../../app");
 
 
-describe('Package endpoint', (done) => {
-    let app;
+describe('Package endpoint', () => {
+    let server;
 
     beforeAll(async () => {
-        app = await create_app();
+        server = await request(await create_app());
+    });
+
+    it('should return package list', async () => {
+        const res = await server.get('/api/package/get');
+        expect(res.status).toBe(200);
+        expect(res.body.length).toBe(7);
     });
 })
