35687b72ea1f034afe21338cd3e50511013bd8cd
ijabir28 <61933818+ijabir28@users.noreply.github.com>
2023-04-12 15:11:39 +0600

    test env updated

--- server/.jest/setEnvVars.js
+++ server/.jest/setEnvVars.js
@@ -1,4 +1,4 @@
-process.env.JWT_SECRET='asdasdjhkjh1273686787ajs;ld0u23oi3jhabkljalsdjkaksdjlasd'
+process.env.JWT_SECRET=''
 process.env.JWT_EXPIRESIN='24h'
 process.env.PORT='4000'
-process.env.MONGO_URL='mongodb+srv://jabir:hard_to_find_me@test.wpszz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+process.env.MONGO_URL='mongodb+srv://<ID>:<PASSWORD>@test.wpszz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
